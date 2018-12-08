0cc5e81ad3f39e3708a5d3f3a3892275055bce08
AmineDev1 <45659795+AmineDev1@users.noreply.github.com>
2018-12-08 12:39:15 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` KSA Community Forever  `,"http://twitch.tv/S-F")
+client.user.setGame(` اكتب اي شي هنا  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
@@ -26,6 +26,52 @@ client.user.setGame(` KSA Community Forever  `,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+
+
+client.on('message',async message => {
+    if(message.content.startsWith(prefix + "bc")) {
+      let filter = m => m.author.id === message.author.id;
+      let thisMessage;
+      let thisFalse;
+      message.channel.send('🇧🇨| **ارسل الرسالة الان**').then(msg => {
+
+      let awaitM = message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      })
+      .then(collected => {
+        collected.first().delete();
+        thisMessage = collected.first().content;
+        msg.edit('🇧🇨| **هل انت متأكد؟**');
+        let awaitY = message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+          max: 1,
+          time: 20000,
+          errors: ['time']
+        })
+        .then(collected => {
+          if(collected.first().content === 'لا') {
+            msg.delete();
+            message.delete();
+            thisFalse = false;
+          }
+          if(collected.first().content === 'نعم') {
+            if(thisFalse === false) return;
+          message.guild.members.forEach(member => {
+            msg.edit('🇧🇨| **جاري الارسال**');
+            collected.first().delete();
+            member.send(`${thisMessage}\n\n${member} ,\nتم الارسال من : ${message.guild.name}\n تم الارسال بواسطة : ${message.author.tag}`);
+          });
+          }
+        });
+      });
+      });
+    }
+  });
+
+
+
+client.login(process.env.BOT_TOKEN);
 
 const Discord = require('discord.js');
 const fs = require('fs');
