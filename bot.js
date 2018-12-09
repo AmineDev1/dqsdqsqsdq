d5bf4ab5433d9739ffedbf54fd1521ebe8dabdf7
AmineDev1 <45659795+AmineDev1@users.noreply.github.com>
2018-12-09 12:42:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,24 @@ client.user.setGame(` KSA Community Forever ♥  `,"http://twitch.tv/S-F")
   console.log('')
 });
 
+
+client.on('message', async msg => {
+const devs = ['417377495160193044'];
+if(!devs.includes(message.author.id)) return;
+  if(msg.author.bot) return;
+  let prefix = "*";
+  if(!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0].slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+  if(command == 'say') {
+    if(!args.join(" ")) return msg.delete();
+    msg.channel.send(args.join(" "));
+    return;
+  }
+});
+
+
 client.on('message', msg => {
 
     if (msg.content == '$join') {
