c32db43dcd4bc654f26405769c1725a0cd3c0874
AmineDev1 <45659795+AmineDev1@users.noreply.github.com>
2018-12-15 13:58:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,24 +67,6 @@ if(!devs.includes(message.author.id)) return;
 });
 
 
-client.on('message', message => { 
-    var prefix = "-";
- let args = message.content.split(' ').slice(1);
-    if(message.content.startsWith(prefix + 's')) {
-    if(!message.channel.guild) return;  
-
-        googl.setKey('AIzaSyC2Z2mZ_nZTcSvh3QvIyrmOIFP6Ra6co6w');
-        googl.getKey();
-        googl.shorten(args.join(' ')).then(shorturl => {
-            message.channel.send(''+shorturl)
-        }).catch(e=>{
-            console.log(e.message);
-            message.channel.send('Error!');
-        });
-}
-});
-
-
 client.on('message', message => {
     if (message.content.startsWith("رابط")) {
 
